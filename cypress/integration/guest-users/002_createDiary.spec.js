805d6b1b627e491bfdc81ac073059a63cab015e5
Daryl <tsdaryl@gmail.com>
2021-07-03 17:08:20 -0700

    create test 002_createDiary.spec.js

---
+++ cypress/integration/guest-users/002_createDiary.spec.js
@@ -0,0 +1,47 @@
+describe("New diary setup - validation", () => {
+  it("diary name cannot be blank", () => {
+    cy.visit("/");
+    cy.get("[data-testid=setup-button").click();
+
+    // try to submit without filling in any information
+    cy.get("[data-testid=create-diary-button").click();
+    cy.get(".form-error").first();
+
+    // fill out information correctly
+    cy.get("#name").click().type("Cypress Test Diary");
+    cy.get("#description")
+      .click()
+      .type("A test diary created using Cypress end-to-end automatic testing.");
+    cy.get("#passcode").click().type("cypress123");
+
+    // submit the form and check that the diary setup page is opened
+    cy.get("[data-testid=create-diary-button").click();
+    cy.contains("Diary Setup");
+    cy.url().should("include", "/setup");
+  });
+
+  it("diary versions can be added without page refresh", () => {
+    cy.get(".tag").should("not.exist");
+    cy.get("#name").click().type("ver x.x.x{enter}");
+    cy.get(".tag");
+    cy.get("[data-testid=next-step").click();
+  });
+
+  it("diary locations can be added without page refresh", () => {
+    cy.get(".tag").should("not.exist");
+    cy.get("#name").click().type("loc1{enter}");
+    cy.get(".tag");
+    cy.get("[data-testid=next-step").click();
+  });
+
+  it("diary types can be added without page refresh", () => {
+    cy.get(".tag").should("not.exist");
+    cy.get("#name").click().type("typ1{enter}");
+    cy.get(".tag");
+    cy.get("[data-testid=next-step").click();
+  });
+
+  it ("should be at the diary page when the last step is complete.", () => {
+    cy.url().should("include", "/diary");
+  })
+});
